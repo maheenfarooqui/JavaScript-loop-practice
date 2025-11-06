8f75d4b65833ed29947e544f8f06c2d4916bbbc3
maheen <maheenzuhra@gmail.com>
2025-11-06 19:29:30 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// ..............................for loop
+for(let i = 0 ; i <= 100; i++){
+    // console.log(i);
+    
+}
+for(let i = 0 ; i >= -100 ; i--){
+    // console.log(i);
+    
+}
+
+const user = ["Afsah", "Hareem", "Hiba", "Nadiya"];
+for(let i = 0 ; i < user.length ; i++){
+    console.log(`Hello ${user[i]} , How Are You ${user[i]} ? `);
+    
+}
+
+const value =[2000,4000,3400,5400];
+let total = 0;
+for(let i = 0 ; i < value.length ; i ++){
+    total += value[i];
+    
+}
+console.log(total);
+
+const value1 = [5000,7600,34567,12340];
+let total1 = 0;
+for(let i = 0 ; i< value1.length ; i++){
+    total1 += value1[i];
+}
+let finalPrice = total1 * 0.9;
+console.log(finalPrice);
+console.log(total1);
+
+
+
+
